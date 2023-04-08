1951d33d696a273a7e7146e0e7bc22b07d1025c4
Ildana <ildana.frolova@yandex.ru>
2023-04-08 17:33:47 +0300

    Второе задание

--- Fierst/src/com/max/idea/Main.java
+++ Fierst/src/com/max/idea/Main.java
@@ -1,6 +1,8 @@
 package com.max.idea;
 
 public class Main {
+    double a = 11.123456;
+    byte b = 127;
     public static void main(String[] args) {
         System.out.println("Hello World");
 
